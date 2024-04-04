0d36d2cd7ed43071283088730de04db966754093
J Cole Morrison <me@jcolemorrison.com>
2024-04-03 21:18:28 -0700

    base function code

---
+++ README.md
@@ -0,0 +1,9 @@
+# Scaling Infrastructure as Code on Google Cloud Platform
+
+The Google Cloud Platform facing cloud infrastructure demo code for the Google Next '24 talk "Scaling Infrastructure as Code: Proven Strategies and Productive Workflows".  
+
+This is the repo used as a template repository in the no-code module and Waypoint workflow for a Google Function.
+
+When the template is used, it will overwrite this README with its own.
+
+It also has a base github actions workflow to seed an initial function for usage when launching the Google Function.  This is required for bootstrapping functions for the first time.
---
+++ index.js
@@ -0,0 +1,46 @@
+const functions = require('@google-cloud/functions-framework');
+const redis = require('redis');
+
+const client = redis.createClient({
+  url: process.env.REDIS_HOST,
+  disableClientInfo: true
+})
+
+client.on('error', err => console.error('ERR:REDIS:', err));
+client.connect();
+
+functions.http('main', async (req, res) => {
+  try {
+    const keys = ["roomcount", "c:game_room"];
+    const keyValuePairs = {};
+
+    for (const key of keys) {
+      const type = await client.type(key);
+
+      switch (type) {
+        case 'string':
+          keyValuePairs[key] = await client.get(key);
+          break;
+        case 'list':
+          keyValuePairs[key] = await client.lRange(key, 0, -1);
+          break;
+        case 'set':
+          keyValuePairs[key] = await client.sMembers(key);
+          break;
+        case 'zset':
+          keyValuePairs[key] = await client.zRange(key, 0, -1);
+          break;
+        case 'hash':
+          keyValuePairs[key] = await client.hGetAll(key);
+          break;
+        default:
+          keyValuePairs[key] = null;
+      }
+    }
+
+    res.send(keyValuePairs);
+  } catch (err) {
+    console.log(err)
+    res.status(500).send('Error retrieving Redis keys and values');
+  }
+});
